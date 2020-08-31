76beeba9bfae45a4ed9950139e9e642e16e4f0cd
hiimchinh <chinhhoang1196@gmail.com>
2020-08-31 11:40:20 +0700

    change to better answer

--- user_input/something.rb
+++ user_input/something.rb
@@ -1,14 +1,10 @@
-question = '>> Do you want me to print something? (y/n)'
+answer = nil
 
 loop do
-  puts question
-  answer = gets.chomp
-  if answer.downcase == 'y'
-    puts 'something'
-    break
-  elsif answer.downcase == 'n'
-    break
-  else
-    puts '>> Invalid input! Please enter y or n'
-  end
+  puts '>> Do you want me to print something? (y/n)'
+  answer = gets.chomp.downcase
+  break if %w(y n).include?(answer)
+  puts '>> Invalid input! Please enter y or n'
 end
+
+puts 'something' if answer == 'y'
