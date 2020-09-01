4d911ade528443fa99d625feb582ac9d63d66ba6
hiimchinh <chinhhoang1196@gmail.com>
2020-09-01 10:44:26 +0700

    add login excercise with constant

---
+++ user_input/login.rb
@@ -0,0 +1,12 @@
+USERNAME = 'admin'
+PASSWORD = 'SecreT'
+
+loop do
+  puts '>> Please enter user name:'
+  input_username = gets.chomp
+  puts '>> Please enter your password:'
+  input_password = gets.chomp
+  break if input_username == USERNAME && input_password == PASSWORD
+  puts '>> Authorization failed!'
+end
+puts 'Welcome!'
