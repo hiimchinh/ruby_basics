452dc559ff1f08f3c9e91ba9e170b4815f28e495
hiimchinh <chinhhoang1196@gmail.com>
2020-09-01 12:11:17 +0700

    refactor with function to loop and validate

--- user_input/opposites.rb
+++ user_input/opposites.rb
@@ -1,28 +1,24 @@
 def valid_number?(number_string)
   number_string.to_i.to_s == number_string && number_string.to_i != 0
 end
-
 first_number = nil
 second_number = nil
+def get_number 
+  loop do
+    puts 'Please enter a positive or negative integer:'
+    input = gets.chomp
+    return input.to_i if valid_number?(input)
+    puts '>> Invalid input. Only non-zero integers are allowed.'
+  end
+end
+
 loop do
-  loop do
-    puts '>> Please enter a positive or a negative number:'
-    first_number = gets.chomp
-    break if valid_number?(first_number)
-    puts '>> Invalid input. Only non-zero integers are allowed.'
-  end
-  loop do
-    puts '>> Please enter a positive or a negative number:'
-    second_number = gets.chomp
-    break if valid_number?(second_number)
-    puts '>> Invalid input. Only non-zero integers are allowed.'
-  end
-  first_number = first_number.to_i
-  second_number = second_number.to_i
-  break if (first_number > 0 && second_number < 0) || (first_number < 0 && second_number > 0)
-  puts 'Sorry. One integer must be positive, one integer must be negative.'
-  puts 'Please start over.'
+  first_number = get_number
+  second_number = get_number
+  break if first_number * second_number < 0
+  puts '>> Sorry. One integer must be positive. one must be negative.'
+  puts '>> Please start again.'
 end
 
 result = first_number + second_number
-  puts "#{first_number} + #{second_number} = #{result}"
+puts "#{first_number} + #{second_number} = #{result}"
