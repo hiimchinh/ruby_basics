68a205c5da1cf72475a59a2e7f32c75a05706f68
hiimchinh <chinhhoang1196@gmail.com>
2020-09-04 18:13:13 +0700

    add pratices string ruby basics

---
+++ strings/ignoring_case.rb
@@ -0,0 +1,5 @@
+name = 'Roger'
+
+puts name.downcase == 'RoGeR'.downcase
+
+puts name.downcase == 'DAVE'.downcase
---
+++ strings/printing_the_alphabet.rb
@@ -0,0 +1,4 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+while alphabet.size > 0
+  puts alphabet.slice!(0)
+end
